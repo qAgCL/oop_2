e00f9eb6e621d5c5e2396ed6cbdf174d363ffe51
Захар Рубанов <agcl3228@gmail.com>
2020-04-02 11:42:09 +0300

    Popitka 322

---
+++ WindowsFormsApp1/Form1.Designer.cs
@@ -0,0 +1,141 @@
+﻿namespace WindowsFormsApp1
+{
+    partial class Form1
+    {
+        /// <summary>
+        /// Обязательная переменная конструктора.
+        /// </summary>
+        private System.ComponentModel.IContainer components = null;
+
+        /// <summary>
+        /// Освободить все используемые ресурсы.
+        /// </summary>
+        /// <param name="disposing">истинно, если управляемый ресурс должен быть удален; иначе ложно.</param>
+        protected override void Dispose(bool disposing)
+        {
+            if (disposing && (components != null))
+            {
+                components.Dispose();
+            }
+            base.Dispose(disposing);
+        }
+
+        #region Код, автоматически созданный конструктором форм Windows
+
+        /// <summary>
+        /// Требуемый метод для поддержки конструктора — не изменяйте 
+        /// содержимое этого метода с помощью редактора кода.
+        /// </summary>
+        private void InitializeComponent()
+        {
+            this.comboBox1 = new System.Windows.Forms.ComboBox();
+            this.button1 = new System.Windows.Forms.Button();
+            this.button2 = new System.Windows.Forms.Button();
+            this.button3 = new System.Windows.Forms.Button();
+            this.button4 = new System.Windows.Forms.Button();
+            this.listBox1 = new System.Windows.Forms.ListBox();
+            this.button5 = new System.Windows.Forms.Button();
+            this.SuspendLayout();
+            // 
+            // comboBox1
+            // 
+            this.comboBox1.DropDownStyle = System.Windows.Forms.ComboBoxStyle.DropDownList;
+            this.comboBox1.Font = new System.Drawing.Font("Microsoft Sans Serif", 12F);
+            this.comboBox1.FormattingEnabled = true;
+            this.comboBox1.Location = new System.Drawing.Point(30, 28);
+            this.comboBox1.Name = "comboBox1";
+            this.comboBox1.Size = new System.Drawing.Size(177, 28);
+            this.comboBox1.TabIndex = 0;
+            // 
+            // button1
+            // 
+            this.button1.Location = new System.Drawing.Point(30, 72);
+            this.button1.Name = "button1";
+            this.button1.Size = new System.Drawing.Size(177, 47);
+            this.button1.TabIndex = 1;
+            this.button1.Text = "Добавить объект";
+            this.button1.UseVisualStyleBackColor = true;
+            this.button1.Click += new System.EventHandler(this.button1_Click);
+            // 
+            // button2
+            // 
+            this.button2.Location = new System.Drawing.Point(30, 134);
+            this.button2.Name = "button2";
+            this.button2.Size = new System.Drawing.Size(177, 47);
+            this.button2.TabIndex = 2;
+            this.button2.Text = "Создать объект";
+            this.button2.UseVisualStyleBackColor = true;
+            this.button2.Click += new System.EventHandler(this.button2_Click);
+            // 
+            // button3
+            // 
+            this.button3.Location = new System.Drawing.Point(30, 200);
+            this.button3.Name = "button3";
+            this.button3.Size = new System.Drawing.Size(177, 47);
+            this.button3.TabIndex = 3;
+            this.button3.Text = "Редактировать объект";
+            this.button3.UseVisualStyleBackColor = true;
+            this.button3.Click += new System.EventHandler(this.button3_Click);
+            // 
+            // button4
+            // 
+            this.button4.Location = new System.Drawing.Point(30, 266);
+            this.button4.Name = "button4";
+            this.button4.Size = new System.Drawing.Size(177, 47);
+            this.button4.TabIndex = 4;
+            this.button4.Text = "Сохранить объект";
+            this.button4.UseVisualStyleBackColor = true;
+            this.button4.Click += new System.EventHandler(this.button4_Click);
+            // 
+            // listBox1
+            // 
+            this.listBox1.Font = new System.Drawing.Font("Microsoft Sans Serif", 12F, System.Drawing.FontStyle.Regular, System.Drawing.GraphicsUnit.Point, ((byte)(204)));
+            this.listBox1.FormattingEnabled = true;
+            this.listBox1.ItemHeight = 20;
+            this.listBox1.Location = new System.Drawing.Point(224, 28);
+            this.listBox1.Name = "listBox1";
+            this.listBox1.Size = new System.Drawing.Size(240, 284);
+            this.listBox1.TabIndex = 5;
+            // 
+            // button5
+            // 
+            this.button5.Location = new System.Drawing.Point(30, 332);
+            this.button5.Name = "button5";
+            this.button5.Size = new System.Drawing.Size(177, 47);
+            this.button5.TabIndex = 6;
+            this.button5.Text = "Удалить объект";
+            this.button5.UseVisualStyleBackColor = true;
+            this.button5.Click += new System.EventHandler(this.button5_Click);
+            // 
+            // Form1
+            // 
+            this.AutoScaleDimensions = new System.Drawing.SizeF(6F, 13F);
+            this.AutoScaleMode = System.Windows.Forms.AutoScaleMode.Font;
+            this.AutoScroll = true;
+            this.ClientSize = new System.Drawing.Size(922, 450);
+            this.Controls.Add(this.button5);
+            this.Controls.Add(this.listBox1);
+            this.Controls.Add(this.button4);
+            this.Controls.Add(this.button3);
+            this.Controls.Add(this.button2);
+            this.Controls.Add(this.button1);
+            this.Controls.Add(this.comboBox1);
+            this.Name = "Form1";
+            this.Text = "Form1";
+            this.Load += new System.EventHandler(this.Form1_Load);
+            this.ResumeLayout(false);
+
+        }
+
+        #endregion
+
+        private System.Windows.Forms.ComboBox comboBox1;
+        private System.Windows.Forms.Button button1;
+        private System.Windows.Forms.Button button2;
+        private System.Windows.Forms.Button button3;
+        private System.Windows.Forms.Button button4;
+        private System.Windows.Forms.ListBox listBox1;
+        private System.Windows.Forms.Button button5;
+    }
+}
+
---
+++ WindowsFormsApp1/Form1.cs
@@ -0,0 +1,387 @@
+﻿using System;
+using System.Collections.Generic;
+using System.ComponentModel;
+using System.Data;
+using System.Drawing;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+using System.Reflection;
+using System.Windows.Forms;
+
+namespace WindowsFormsApp1
+{
+    using Classes;
+    public partial class Form1 : Form
+    {
+        public Form1()
+        {
+            InitializeComponent();
+        }
+        public static int printField(Type type1, List<Control> controls, int x, int y)
+        {
+            FieldInfo[] fieldinfo1 = type1.GetFields();
+            foreach (var field1 in fieldinfo1)
+            {
+                Type type2 = Type.GetType(field1.FieldType.ToString());
+                if (type2.IsEnum)
+                {
+                    controls.Add(new Label() { Font = new Font("Microsoft Sans Serif", 12), Size = new Size(300, 20), Location = new Point(0 + x, 10 + y * 28), Text = type1.Name + "   " + field1.Name });
+                    y++;
+                    ComboBox buf = new ComboBox() { Font = new Font("Microsoft Sans Serif", 12), Size = new Size(300, 28), DropDownStyle = System.Windows.Forms.ComboBoxStyle.DropDownList, Name = field1.Name, Location = new Point(0 + x, 10 + y * 28) };
+                    FieldInfo[] fieldinfo2 = type2.GetFields(BindingFlags.Public | BindingFlags.Static);
+                    foreach (var field2 in fieldinfo2)
+                    {
+                        buf.Items.Add(field2.Name.ToString());
+                    }
+                    controls.Add(buf);
+                    y++;
+                }
+                else if ((type2.IsClass) && (type2.Name != "String"))
+                {
+                    y = printField(field1.FieldType, controls, x, y);
+                }
+                else
+                {
+                    controls.Add(new Label() { Font = new Font("Microsoft Sans Serif", 12), Size = new Size(300, 20), Location = new Point(0 + x, 10 + y * 28), Text = type1.Name + "   " + field1.Name });
+                    y++;
+                    controls.Add(new TextBox() { Font = new Font("Microsoft Sans Serif", 12), Size = new Size(300, 28), Name = field1.Name, Location = new Point(0 + x, 10 + y * 28) });
+                    y++;
+                }
+               
+            }
+            return y;
+        }
+        public static int printVal(Object obj,List<Control> controls, int x, int y)
+        {
+            if (obj != null)
+            {
+                Type type1 = obj.GetType();
+                Console.WriteLine(type1.Name);
+                FieldInfo[] fieldinfo1 = type1.GetFields();
+                foreach (var field1 in fieldinfo1)
+                {
+                    Type type2 = Type.GetType(field1.FieldType.ToString());
+                    if (type2.IsEnum)
+                    {
+                        controls.Add(new Label() { Font = new Font("Microsoft Sans Serif", 12), Size = new Size(300, 20), Location = new Point(0 + x, 28 + y * 30), Text = type1.Name + "   " + field1.Name });
+                        y++;
+                        ComboBox buf = new ComboBox() { Font = new Font("Microsoft Sans Serif", 12), Size = new Size(300, 28), DropDownStyle = System.Windows.Forms.ComboBoxStyle.DropDownList, Name = field1.Name, Location = new Point(0 + x, 28 + y * 30) };
+                        FieldInfo[] fieldinfo2 = type2.GetFields(BindingFlags.Public | BindingFlags.Static);
+                        foreach (var field2 in fieldinfo2)
+                        {
+                            buf.Items.Add(field2.Name.ToString());
+                        }
+                        buf.Text = field1.GetValue(obj).ToString();
+                        controls.Add(buf);
+                        y++;
+                    }
+                    else if ((type2.IsClass) && (type2.Name != "String"))
+                    {
+                        y = printVal(field1.GetValue(obj), controls, x, y);
+                    }
+                    else
+                    {
+                        controls.Add(new Label() { Font = new Font("Microsoft Sans Serif", 12), Size = new Size(300, 20), Location = new Point(0 + x, 28 + y * 30), Text = type1.Name + "   " + field1.Name });
+                        y++;
+                        controls.Add(new TextBox() { Text = field1.GetValue(obj).ToString(), Font = new Font("Microsoft Sans Serif", 12), Size = new Size(300, 28), Name = field1.Name, Location = new Point(0 + x, 28 + y * 30) });
+                        y++;
+                    }
+                }
+            }
+            return y;
+        }
+        public static bool setVal(Object obj,List<Control> controls, List<Object> objects)
+        {
+            if (obj != null)
+            {
+                Type type1 = obj.GetType();
+                objects.Add(obj);
+                FieldInfo[] fieldinfo1 = type1.GetFields();
+                foreach (var field1 in fieldinfo1)
+                {
+                    Type type2 = Type.GetType(field1.FieldType.ToString());
+                    if ((type2.IsClass) && (type2.Name != "String"))
+                    {
+                        setVal(field1.GetValue(obj), controls, objects);
+                    }
+                    else if (type2.IsEnum)
+                    {
+                        FieldInfo[] fieldinfo2 = type2.GetFields(BindingFlags.Public | BindingFlags.Static);
+                        bool flag = false;
+                        foreach (Control control in controls)
+                        {
+                            if (control.Name.ToString() == field1.Name.ToString())
+                            {
+                                Object val = control.Text;
+                                foreach (var field2 in fieldinfo2)
+                                {
+                                    if (val.ToString() == field2.Name)
+                                    {
+                                        field1.SetValue(obj, field2.GetValue(obj));
+                                    }
+                                }
+                            }
+                        }
+                        if (flag)
+                        {
+                            MessageBox.Show("Неправильно заполнено поле " + field1.Name);
+                            return false;
+                        }
+                    }
+                    else
+                    {
+                        Object val = 0;
+                        foreach (Control control in controls)
+                        {
+                            if (control.Name.ToString() == field1.Name.ToString())
+                            {
+                                val = control.Text;
+                            }
+                        }
+                        try
+                        {
+                            val = Convert.ChangeType(val, field1.FieldType);
+                        }
+                        catch
+                        {
+                            MessageBox.Show("Неправильно заполнено поле " + field1.Name);
+                            return false;
+                        }
+                        field1.SetValue(obj, val);
+                    }
+                }
+            }
+            return true;
+        }
+        public static Object create_obj(Type loc_type)
+        {
+            ConstructorInfo[] cons = loc_type.GetConstructors();
+            ParameterInfo[] pars = cons[0].GetParameters();
+            List<Object> test = new List<Object>();
+            if (pars.Length == 0)
+            {
+                return Activator.CreateInstance(loc_type);
+            }
+            else
+            {
+                foreach (var para in pars)
+                {
+                    test.Add(create_obj(para.ParameterType));
+                }
+            }
+            return Activator.CreateInstance(loc_type, test.ToArray());
+        }
+        private void Form1_Load(object sender, EventArgs e)
+        {
+            Type[] typelist = Assembly.GetExecutingAssembly().GetTypes().Where(t => t.Namespace == "Classes").ToArray();
+            foreach (Type type in typelist)
+            {
+               
+                if (type.IsClass)
+                {
+                    comboBox1.Items.Add(type.Name);
+                }
+            }
+        }
+        List<Control> controls = new List<Control>();
+        Object bufObject;
+        List<Object> allObj = new List<Object>();
+
+        private void button1_Click(object sender, EventArgs e)
+        {
+            foreach (Control control in controls)
+            {
+                this.Controls.Remove(control);
+            }
+            controls.Clear();
+            bufObject = create_obj(Type.GetType("Classes."+comboBox1.Text));
+            printField(bufObject.GetType(), controls, 482, 0);
+            foreach (Control control in controls)
+            {
+                this.Controls.Add(control);
+            }
+        }
+
+        private void button2_Click(object sender, EventArgs e)
+        {
+            List<Object> loc_arrObj = new List<Object>();
+            if (setVal(bufObject, controls, loc_arrObj))
+            {
+                foreach (Control control in controls)
+                {
+                    this.Controls.Remove(control);
+                }
+                controls.Clear();
+                int count = allObj.Count();
+                foreach (Object obj in loc_arrObj)
+                {
+                    allObj.Add(obj);
+                    listBox1.Items.Add(obj.ToString()+"_"+count.ToString());
+                }
+            }
+        }
+
+        private void button3_Click(object sender, EventArgs e)
+        {
+            foreach (Control control in controls)
+            {
+                this.Controls.Remove(control);
+            }
+            controls.Clear();
+            printVal(allObj[listBox1.SelectedIndex], controls, 482, 0);
+            foreach (Control control in controls)
+            {
+                this.Controls.Add(control);
+            }
+        }
+
+        private void button4_Click(object sender, EventArgs e)
+        {
+            List<Object> loc_arrObj = new List<Object>();
+            if (setVal(allObj[listBox1.SelectedIndex], controls, loc_arrObj))
+            {
+                foreach (Control control in controls)
+                {
+                    this.Controls.Remove(control);
+                }
+                controls.Clear();
+            }
+        }
+
+        private void button5_Click(object sender, EventArgs e)
+        {
+            allObj.RemoveAt(listBox1.SelectedIndex);
+            listBox1.Items.RemoveAt(listBox1.SelectedIndex);
+        }
+    }
+}
+namespace Classes
+{
+    enum yes_no
+    {
+        Yes,
+        No,
+    }
+    class airplane : transport
+    {
+       // public int flight_height;
+        public int wingsspan;
+        //  public int wing_area;
+        public yes_no auto_pilot;
+        public engine engine;
+        public airplane(engine eng)
+        {
+            engine = eng;
+        }
+    }
+    class auto_transport : transport
+    {
+        public enum drive
+        {
+            Front_wheel_drive,
+            Real_wheel_drive,
+            Four_wheel_drive,
+        }
+        // public int number_of_wheels;
+        //public int tire_diameter;
+        public int tire_width;
+        //  public int tire_pressure;
+        public drive drive_unit;
+    }
+    class carpark
+    {
+        public int area;
+        //   public int car_capacity;
+        //    public int number_of_cars;
+        //    public int number_of_workers;
+        public int profit;
+        public yes_no car_wash;
+        public yes_no gas_station;
+        public yes_no service_station;
+        public ice_auto auto;
+        public employee employee;
+        public carpark(employee emp, ice_auto aut)
+        {
+            auto = aut;
+            employee = emp;
+        }
+    }
+    class electro_auto : auto_transport
+    {
+        enum battery
+        {
+
+            Lithium_ion,
+            Aluminum_ion,
+            Lithium_sulfur,
+
+        }
+        //  public int battery_capacity;
+        public int charging_time;
+        //  public int power_reserve;
+        public int battery_consumption;
+        public yes_no quick_charge;
+        public engine engine;
+        public electro_auto(engine eng)
+        {
+            engine = eng;
+        }
+    }
+    class employee
+    {
+        public enum Gender
+        {
+            Female,
+            Male,
+            Agender,
+            Bigender,
+        }
+        public int height;
+        //    public int weight;
+        public int age;
+        //     public string education;
+        //     public string name;
+        public string surname;
+        public engine engine;
+        public Gender gender;
+        public int salary;
+    }
+    public class engine
+    {
+        public int weight;
+        public int year_of_creation;
+        public int service_life;
+        public int power;
+        public int torque;
+    }
+    abstract class transport
+    {
+        //   public int length;
+        public int weight;
+        public int height;
+        // public int width;
+        //    public int carrying;
+        public string color;
+    }
+    class ice_auto : auto_transport
+    {
+        public enum fuel
+        {
+            Gas,
+            Petrol,
+            Diesel,
+        }
+        // public int gas_tank_volume;
+        // public int fuel_consumption;
+        public int oil_consumption;
+        // public int co2_emissions;
+        public fuel fuel_grade;
+        public engine engine;
+        public ice_auto(engine eng)
+        {
+            engine = eng;
+        }
+    }
+}
+
